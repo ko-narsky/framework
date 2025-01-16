13d95ea2cbe474aecb1f9bf95a0a7be1d5bd4159
r.konyushenny <r.konyushenny@efko.ru>
2025-01-16 23:50:26 +0300

    [KONARSKY-48] - Исправление конфига ресурса

--- src/HTTP/Router/Resource.php
+++ src/HTTP/Router/Resource.php
@@ -11,59 +11,18 @@ readonly class Resource
     /**
      * @param string $name
      * @param string $controller
-     * @param ?array $config
+     * @param array $config
      */
     public function __construct(
         private string $name,
         private string $controller,
-        private ?array $config
+        private array  $config = []
     ) { }
 
     public function build(HTTPRouterInterface $router): Route
     {
         $path = $this->name;
-        $config = $this->config ??
-            [
-                'index' => [
-                    'method' => 'GET',
-                    'path' => $path,
-                    'action' => 'actionList',
-                    'middleware' => [],
-                ],
-                'view' => [
-                    'method' => 'GET',
-                    'path' => "{$path}/{:id|integer}",
-                    'action' => 'actionView',
-                    'rules' => [
-                        'id:required|integer',
-                    ],
-                    'middleware' => [],
-                ],
-                'create' => [
-                    'method' => 'POST',
-                    'path' => $path,
-                    'action' => 'actionCreate',
-                    'middleware' => [],
-                ],
-                'put' => [
-                    'method' => 'PUT',
-                    'path' => "{$path}/{:id|integer}",
-                    'action' => 'actionUpdate',
-                    'middleware' => [],
-                ],
-                'patch' => [
-                    'method' => 'PATCH',
-                    'path' => "{$path}/{:id|integer}",
-                    'action' => 'actionPatch',
-                    'middleware' => [],
-                ],
-                'delete' => [
-                    'method' => 'DELETE',
-                    'path' => "{$path}/{:id|integer}",
-                    'action' => 'actionDelete',
-                    'middleware' => [],
-                ],
-            ];
+        $config = $this->setConfig($path);
 
         foreach ($config as $params) {
             $route = $router->add($params['method'], $params['path'], $this->controller . '::' . $params['action']);
@@ -78,5 +37,59 @@ readonly class Resource
         }
 
         return $router->group($path);
+    }
+
+    private function setConfig($path): array
+    {
+        $config = [
+            'index' => [
+                'method' => 'GET',
+                'path' => $path,
+                'action' => 'actionList',
+                'middleware' => [],
+            ],
+            'view' => [
+                'method' => 'GET',
+                'path' => "{$path}/{:id|integer}",
+                'action' => 'actionView',
+                'middleware' => [],
+            ],
+            'create' => [
+                'method' => 'POST',
+                'path' => $path,
+                'action' => 'actionCreate',
+                'middleware' => [],
+            ],
+            'put' => [
+                'method' => 'PUT',
+                'path' => "{$path}/{:id|integer}",
+                'action' => 'actionUpdate',
+                'middleware' => [],
+            ],
+            'patch' => [
+                'method' => 'PATCH',
+                'path' => "{$path}/{:id|integer}",
+                'action' => 'actionPatch',
+                'middleware' => [],
+            ],
+            'delete' => [
+                'method' => 'DELETE',
+                'path' => "{$path}/{:id|integer}",
+                'action' => 'actionDelete',
+                'middleware' => [],
+            ],
+        ];
+
+        foreach ($this->config as $method => $newElement) {
+            if (isset($config[$method]) === false) {
+                $config[$method] = $newElement;
+
+                continue;
+            }
+
+            $config[$method] = array_merge($config[$method], $newElement);
+        }
+
+        return $config;
     }
 }
