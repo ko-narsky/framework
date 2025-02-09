92ca7bb9a45992a20a26b4fea53efe427b65fa32
k.kenarskih <k.kenarskih@efko.ru>
2025-02-09 19:23:33 +0300

    [KONARSKY-71] Правка исключения

--- src/Exception/Resource/BadRequestResourceException.php
+++ src/Exception/Resource/BadRequestResourceException.php
@@ -3,12 +3,13 @@
 namespace Konarsky\Exception\Resource;
 
 use Konarsky\Exception\Resource\ResourceException;
+use Throwable;
 
 class BadRequestResourceException extends ResourceException
 {
-    public function __construct() {
+    public function __construct(string $message = 'Контракт не соответствует требованиям') {
         parent::__construct(json_encode([
-            'cause' => 'Контракт не соответствует требованиям',
+            'cause' => $message,
             'type' => 'BadRequest'
         ], JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE));
     }
